21c1655605679c3f07d1f02d0e042ec418844db8
EC2 Default User <ec2-user@ip-172-31-68-230.ec2.internal>
2022-08-23 20:35:23 +0000

    initial commit

---
+++ AWS_Service_Inventory_list.py
@@ -0,0 +1,13 @@
+# AWS Service inventory - Create a list of AWS services using Python
+
+# Create intial empty list
+aws_services = []
+
+# Populate the list with AWS services
+for service in ["S3", "VPC", "RDS", "DynamoDB", "EC2", "CloudWatch"]:
+    aws_services.append(service)
+    
+# Print the list & list length
+print(aws_services)
+print(len(aws_services))
+
