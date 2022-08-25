b308455f5063a5db2c37ea2f6a05c9c1a16894f4
EC2 Default User <ec2-user@ip-172-31-68-230.ec2.internal>
2022-08-25 15:07:09 +0000

    Added some comments and text to outputs

--- AWS_Service_Inventory_list.py
+++ AWS_Service_Inventory_list.py
@@ -9,12 +9,12 @@ for service in ["S3", "VPC", "RDS", "DynamoDB", "EC2", "CloudWatch"]:
     
 # Print the list & list length
 print(aws_services)
-print(len(aws_services))
+print("These are currently " + str(len(aws_services)) + " services in the service inventory.")
 
 # Remove remove two services from the list
-aws_services.pop()
-aws_services.pop(2)
+aws_services.pop() #removes the last element in the list
+aws_services.pop(2) #removes the 3rd element in the list
 
 # Print the list & list length
 print(aws_services)
-print(len(aws_services))
+print("There are currently " + str(len(aws_services)) + " services remaining in the inventory.")
