4a364e8cb585963502317a167c5f7f185581ec84
EC2 Default User <ec2-user@ip-172-31-68-230.ec2.internal>
2022-08-28 19:35:03 +0000

    initial commit

---
+++ Ec2_random_name_gen.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+
+import string
+import random
+
+EC2 = []
+deptartent = ['Marketing', 'Accounting', 'FinOps']
+for i in deptartent:
+    if i.lower() or i.upper() not in dept:
+        print("Dear user, you should not use the Name Genarator")
+    else:
+        
