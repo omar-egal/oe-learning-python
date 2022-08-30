f1d5c987c7c0118671f86ac85bc327b3991fa009
EC2 Default User <ec2-user@ip-172-31-68-230.ec2.internal>
2022-08-30 22:27:40 +0000

    Current commit, need to add logic to have exceptions

--- Ec2_random_name_gen.py
+++ Ec2_random_name_gen.py
@@ -4,18 +4,26 @@ import string, random
 from itertools import repeat
 
 #Departments 
-#departments = ('Marketing', 'Accounting', 'FinOps')
+departments =  ['Marketing', 'Accounting', 'FinOps']
 
-#Input number of instances and dpeartment name
+#INumber of instances and dpeartment name
 number_of_instances = int(input("Please enter the number of EC2 instnaces required: "))
 department = str(input("Please enter a department name: "))
 
-#function
+#function to add random numbers and characters to department name as EC2 instance name
 def getName():
     output = ''
     for _ in repeat(None, number_of_instances):
             extra = ''.join(random.choice(string.ascii_letters + string.digits) for i in range(8))
             output = department + '_' + extra
             return output
+            
+
+#Loop to repeat fuction for the number of instances entered
 for i in repeat(None, number_of_instances):
-    print(getName())
+    deny = [x for x in departments if(x in departments)]
+    if deny == True:
+        print("Access denied")
+    else:
+        print(getName())
+    
