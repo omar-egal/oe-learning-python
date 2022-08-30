96c0ca50c0cc31add66be519a36d08385acc746b
EC2 Default User <ec2-user@ip-172-31-68-230.ec2.internal>
2022-08-30 19:54:35 +0000

    new version of code, random gen appends chars & digits to 1 instance

--- Ec2_random_name_gen.py
+++ Ec2_random_name_gen.py
@@ -2,27 +2,20 @@
 
 import string, random
 
-#Initial list of departments
-departments = ('Marketing', 'Accounting', 'FinOps')
+#Departments 
+#departments = ('Marketing', 'Accounting', 'FinOps')
 
 #Input number of instances and dpeartment name
-number_of_instances = int(input("Please enter the number of EC2 instnaces: "))
-department = str(input("Please enter a department name: "))
-#function to search item
-def getDepartment():
-   for i in departments:
-       if department.casefold()==i.casefold():
-           return True
- 
-   else:
-       return False
- 
-if getDepartment():
-   
-  # If function returns true
-   print('The department that you have entered is present')
- 
-else:
-   
-  # If function returns false
-   print('Classroom you are searching is not present')
+
+
+#function
+def getName():
+    output = ''
+    number_of_instances = int(input("Please enter the number of EC2 instnaces required: "))
+    department = str(input("Please enter a department name: "))
+    while number_of_instances > 0:
+        extra = ''.join(random.choice(string.ascii_letters + string.digits) for i in range(8))
+        output = department + extra
+        number_of_instances -= 1
+    return output
+print(getName())
