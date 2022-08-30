6f13c50c86ee2762619ae1fb6b8db19012729760
EC2 Default User <ec2-user@ip-172-31-68-230.ec2.internal>
2022-08-30 20:28:06 +0000

    Modified fuction to generate names based on the number of instances required

--- Ec2_random_name_gen.py
+++ Ec2_random_name_gen.py
@@ -1,21 +1,21 @@
 #!/usr/bin/env python3
 
 import string, random
+from itertools import repeat
 
 #Departments 
 #departments = ('Marketing', 'Accounting', 'FinOps')
 
 #Input number of instances and dpeartment name
-
+number_of_instances = int(input("Please enter the number of EC2 instnaces required: "))
+department = str(input("Please enter a department name: "))
 
 #function
 def getName():
     output = ''
-    number_of_instances = int(input("Please enter the number of EC2 instnaces required: "))
-    department = str(input("Please enter a department name: "))
-    while number_of_instances > 0:
-        extra = ''.join(random.choice(string.ascii_letters + string.digits) for i in range(8))
-        output = department + extra
-        number_of_instances -= 1
-    return output
-print(getName())
+    for _ in repeat(None, number_of_instances):
+            extra = ''.join(random.choice(string.ascii_letters + string.digits) for i in range(8))
+            output = department + extra
+            return output
+for i in repeat(None, number_of_instances):
+    print(getName())
