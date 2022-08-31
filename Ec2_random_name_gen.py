d29396655fa0967861f6934fc3dc1de7f4d0e6fc
EC2 Default User <ec2-user@ip-172-31-68-230.ec2.internal>
2022-08-31 20:20:33 +0000

    Added comments

--- Ec2_random_name_gen.py
+++ Ec2_random_name_gen.py
@@ -1,5 +1,16 @@
 #!/usr/bin/env python3
 
+#Several departments share an AWS environment. 
+#You need to ensure that the EC2s are properly named and are unique so team members can easily tell who the EC2 instances belong to. 
+#Use Python to create your unique EC2 names that the users can then attach to the instances.
+#1. All the user to input how many EC2 instances they want names for and output the same amount of unique names.
+#2. Allow the user to input the name of their department that is used in the unique name.
+#3. Generate random characters and numbers that will be included in the unique name.
+#The only departments that should use this Name Generator are the Marketing, Accounting, and FinOps Departments. 
+#List these departments as options and if a user puts another department, print a message that they should not use this Name Generator. 
+#Be sure to account for incorrect upper or lowercase letters in the correct department. 
+#Example: a user inputs accounting vs Accounting.
+
 import string, random
 from itertools import repeat
 
