9edaf7eec12d1a5724c8abd66f388da35fc1720e
EC2 Default User <ec2-user@ip-172-31-68-230.ec2.internal>
2022-09-05 08:26:50 +0000

    added comment

--- boto3-practice4.py
+++ boto3-practice4.py
@@ -1,5 +1,5 @@
 import boto3
 s3_resource = boto3.client('s3')
+#list all bucket names and their creation dates
 for bucket in s3_resource.list_buckets()["Buckets"]:
     print("S3 Bucket:",bucket['Name']," Creation date:", bucket['CreationDate'])
- #   print(bucket['CreationDate'])
