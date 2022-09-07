64898cc5b8429a115d12d3b1712493b926d486ef
EC2 Default User <ec2-user@ip-172-31-68-230.ec2.internal>
2022-09-07 18:02:20 +0000

    added code to stop only running instances in Dev environment

--- get_ec2_instances.py
+++ get_ec2_instances.py
@@ -1,4 +1,6 @@
 import boto3
+
+
 #Describe instances. Filter for running instances in the dev Environment
 client = boto3.client('ec2')
 running_dev_clients = client.describe_instances(
@@ -9,4 +11,20 @@ running_dev_clients = client.describe_instances(
                 'Name': 'tag:Environment', 'Values': ['dev']
         }
     ])
-print(running_dev_clients)
+
+#loop to get instance IDs for the running dev instances
+dev_ids = []
+for reservation in running_dev_clients['Reservations']:
+    for instance in reservation['Instances']:
+        dev_id = instance['InstanceId']
+        dev_ids.append(dev_id)
+    print(dev_ids)
+
+
+#Stop any running dev instnaces
+for i_d in dev_ids:
+    response = client.stop_instances(
+    InstanceIds=[
+        i_d,
+    ],
+)
